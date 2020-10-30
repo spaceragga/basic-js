acb567f6bb4f352c7c8911f1da68da02dec146c7
Ivan <raggamix@mail.ru>
2020-10-30 22:10:09 +0300

    feat: implement 2 task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] === '--discard-next') {
+     i++;
+     continue;
+    }
+    if(arr[i] === '--discard-prev') {
+      if (result.length !== 0 && arr[i - 2] !== '--discard-next') result.pop();
+      continue;
+    }
+    if(arr[i] === '--double-next') {
+      if (i < arr.length - 1) result.push(arr[i + 1]);
+      continue;
+    }
+    if(arr[i] === '--double-prev') {
+      if (i !== 0 && arr[i - 2] !== '--discard-next') result.push(arr[i - 1]);
+      continue;
+    }
+    result.push(arr[i]);
+  }
+  return result;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date) {
+    if (Date.prototype.isPrototypeOf(date) && !isNaN(date)) {
+      let month=date.getMonth();
+        if(month >= 0 && month < 2||month === 11) return 'winter';
+        if(month >= 2 && month < 5) return 'spring';
+        if(month >= 5 && month < 8) return 'summer';
+        if(month >= 8 && month < 11) return 'autumn';
+        else '';
+    } else {
+        throw new Error('Error');
+    }
+  } else {
+    return 'Unable to determine the time of year!';
+  }
 };
