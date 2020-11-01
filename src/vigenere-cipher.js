f72656b5f1e3f6aa3b1e08357ffc3e922d1facfb
Ivan <raggamix@mail.ru>
2020-11-01 21:38:03 +0300

    feat: vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
+let alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+    constructor(reverse) {
+      console.log(reverse);
+      if (reverse === false) this.reverse = false;
+      else this.reverse = true;
+    }
+  
+    encrypt(string, key) {
+      if (typeof string === 'undefined' || typeof key === 'undefined') throw new Error();
+      else {
+        string = string.toUpperCase();
+        key = key.toUpperCase();
+        while (key.length < string.length) {
+          key += key;
+        }
+        let result = '';
+        for (let i = 0, j = 0; i < string.length; i++, j++) {
+          if (alp.includes(string.charAt(i))) {
+            result += alp.charAt((alp.indexOf(string.charAt(i)) + alp.indexOf(key.charAt(j))) % alp.length);
+          } else {
+            result += string.charAt(i);
+            j--;
+          }
+        }
+        if (this.reverse === false) result = result.split('').reverse().join('');
+        return result;
+      }
+    }
+  
+    decrypt(string, key) {
+      if (typeof string === 'undefined' || typeof key === 'undefined') throw new Error();
+      else {
+        string = string.toUpperCase();
+        key = key.toUpperCase();
+        while (key.length < string.length) {
+          key += key;
+        }
+        let result = '';
+        for (let i = 0, j = 0; i < string.length; i++, j++) {
+          if (alp.includes(string.charAt(i))) {
+            result += alp.charAt((alp.indexOf(string.charAt(i)) - alp.indexOf(key.charAt(j)) + alp.length) % alp.length);
+          } else {
+            result += string.charAt(i);
+            j--;
+          }
+        }
+        if (this.reverse === false) result = result.split('').reverse().join('');
+        return result;
+      }
+    }
   }
-}
+
 
 module.exports = VigenereCipheringMachine;
